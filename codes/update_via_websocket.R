4ec4f0cbfe55877c3c51c9b62b2522ecf8f0a223
DivadNojnarg <dgranjon@ymail.com>
2020-08-23 16:55:36 +0200

    update input with the websocket (shunt the UI interaction)

---
+++ codes/update_via_websocket.R
@@ -0,0 +1,34 @@
+library(shiny)
+
+updateObsVal <- function(value) {
+  sprintf(
+    "Shiny.shinyapp.$sendMsg(JSON.stringify({
+      method: 'update',
+      data: {obs: %s}
+    }));",
+    value
+  )
+}
+
+# below we shunt the slider input by sending message
+# directly through the websocket
+
+ui <- fluidPage(
+  actionButton(
+    "plop",
+    "Update obs value",
+    onclick = updateObsVal(4)
+  ),
+  sliderInput("obs", "Number of observations:",
+              min = 0, max = 1000, value = 500
+  ),
+  plotOutput("distPlot")
+)
+
+server <- function(input, output, session) {
+  output$distPlot <- renderPlot({
+    hist(rnorm(input$obs))
+  })
+}
+
+shinyApp(ui, server)

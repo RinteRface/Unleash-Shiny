4f5a6998459ea522d2f7d1444c52cf2e88a4b2db
JohnCoene <jcoenep@gmail.com>
2020-09-20 10:53:05 +0200

    add custom output

---
+++ output/app.R
@@ -0,0 +1,101 @@
+library(shiny)
+library(shinybulma)
+
+boxxy <- function(title, value, color = NULL, animate = TRUE){
+
+  value <- sum(value)
+
+  # dynamic color
+  if(is.null(color))
+    if(value > 100)
+      color <- "#ef476f"
+    else
+      color <- "#06d6a0"
+
+  list(title = title, value = value, color = color, animate = animate)
+}
+
+boxxyOutput <- function(id){
+  el <- shiny::tags$div(
+    id = id, class = "box boxxy",
+    div(
+      class = "level-item has-text-centered",
+      div(
+        h1(id = sprintf("%s-boxxy-value", id), class = "heading boxxy-value"),
+        p(id = sprintf("%s-boxxy-title", id), class = "title boxxy-title")
+      )
+    )
+  )
+
+  path <- normalizePath("assets")
+
+  deps <- list(
+    htmltools::htmlDependency(
+      name = "boxxy",
+      version = "1.0.0",
+      src = c(file = path),
+      script = c("binding.js"), # only binding
+      stylesheet = "styles.css"
+    )
+  )
+
+  htmltools::attachDependencies(el, deps)
+}
+
+renderBoxxy <- function(expr, env = parent.frame(), quoted = FALSE) {
+  # Convert the expression + environment into a function
+  func <- shiny::exprToFunction(expr, env, quoted)
+
+  function(){
+    val <- func()
+
+    if(val$animate){
+      path <- normalizePath("assets")
+
+      deps <- htmltools::htmlDependency(
+        name = "countup",
+        version = "1.8.2",
+        src = c(file = path),
+        script = c("countup.js") # only countup
+      )
+
+      val$deps <- lapply(
+        htmltools::resolveDependencies(list(deps)),
+        shiny::createWebDependency
+      )
+    }
+
+    return(val)
+  }
+}
+
+ui <- bulmaPage(
+  bulmaContainer(
+    bulmaColumns(
+      bulmaColumn(boxxyOutput("countries")),
+      bulmaColumn(boxxyOutput("employees")),
+      bulmaColumn(boxxyOutput("customers")),
+      bulmaColumn(boxxyOutput("subs"))
+    )
+  )
+)
+
+server <- function(input, output){
+  output$countries <- renderBoxxy({
+    boxxy("Countries", 95, color = "#ef476f", animate = FALSE)
+  })
+
+  output$employees <- renderBoxxy({
+    boxxy("Thing", 650, color = "#06d6a0")
+  })
+
+  output$customers <- renderBoxxy({
+    boxxy("Customers", 13592, color = "#118ab2")
+  })
+
+  output$subs <- renderBoxxy({
+    boxxy("Subscriptions", 16719, color = "#ffd166")
+  })
+}
+
+shinyApp(ui, server)

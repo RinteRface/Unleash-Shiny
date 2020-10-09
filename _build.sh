eeef365c93bbd97e7131c15376577c3f839d0e99
JohnCoene <jcoenep@gmail.com>
2020-10-09 18:17:18 +0200

    delete build.sh

--- _build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-Rscript -e "rmarkdown::render('./slides/index.Rmd')"
-rm -rf ./website/slides
-cp -r ./slides ./website/slides
-cd ./website && Rscript -e "rmarkdown::render_site()" && cd -
